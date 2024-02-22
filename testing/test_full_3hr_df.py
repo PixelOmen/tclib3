5cf11c4edc23fc744f14288ab18e250bb76ad583
Eman <eacosta@roundabout.com>
2024-02-21 18:10:59 -0800

    Switched to pytest framework, added more unit tests

--- testing/test_full_3hr_df.py
+++ testing/test_full_3hr_df.py
@@ -1,6 +1,8 @@
 import sys
-import unittest
 from pathlib import Path
+
+import pytest 
+pytestmark = pytest.mark.skip(reason="This test is slow and not does not need to be run every time. Comment this to run.")
 
 HERE = Path(__file__).parent
 ROOT = HERE.parent
@@ -9,41 +11,37 @@ if ROOT not in sys.path:
 
 from src import tclib3
 
-class Test3HourDF(unittest.TestCase):
-    def test_df_every_tc_to_hr3(self):
-        test_fps = round(29.97)
-        start_tc = "00:00:00:00"
-        tc_split = start_tc.split(":")
-        while True:
-            hrs = int(tc_split[0])
-            mins = int(tc_split[1])
-            secs = int(tc_split[2])
-            frames = int(tc_split[3])
+def test_df_every_tc_to_hr3():
+    test_fps = round(29.97)
+    start_tc = "00:00:00:00"
+    tc_split = start_tc.split(":")
+    while True:
+        hrs = int(tc_split[0])
+        mins = int(tc_split[1])
+        secs = int(tc_split[2])
+        frames = int(tc_split[3])
 
-            try:
-                tc_str = ":".join(tc_split)
-                tc_str = tc_str[:8] + ";" + tc_str[9:]
-                test_frames = tclib3.tc_to_frames(tc_str, 29.97)
-                test_string = tclib3.frames_to_tc(test_frames, 29.97, True)
-                reverted_frames = tclib3.tc_to_frames(test_string, 29.97)
-                self.assertEqual(test_frames, reverted_frames)
-            except ValueError as e:
-                if not str(e).startswith("Invalid frame"):
-                    raise ValueError(e)
+        try:
+            tc_str = ":".join(tc_split)
+            tc_str = tc_str[:8] + ";" + tc_str[9:]
+            test_frames = tclib3.tc_to_frames(tc_str, 29.97)
+            test_string = tclib3.frames_to_tc(test_frames, 29.97, True)
+            reverted_frames = tclib3.tc_to_frames(test_string, 29.97)
+            assert test_frames == reverted_frames
+        except ValueError as e:
+            if str(e).startswith("Invalid frame"):
+                pass
 
-            frames += 1
-            if frames >= test_fps:
-                frames = 0
-                secs += 1
-            if secs >= 60:
-                secs = 0
-                mins += 1
-            if mins >= 60:
-                mins = 0
-                hrs += 1
-            tc_split = [f"{hrs:02}", f"{mins:02}", f"{secs:02}", f"{frames:02}"]
-            if hrs >= 3:
-                break
-
-if __name__ == "__main__":
-    unittest.main()
+        frames += 1
+        if frames >= test_fps:
+            frames = 0
+            secs += 1
+        if secs >= 60:
+            secs = 0
+            mins += 1
+        if mins >= 60:
+            mins = 0
+            hrs += 1
+        tc_split = [f"{hrs:02}", f"{mins:02}", f"{secs:02}", f"{frames:02}"]
+        if hrs >= 3:
+            break
--- testing/test_tclib3.py
+++ testing/test_tclib3.py
@@ -1,6 +1,7 @@
 import sys
-import unittest
 from pathlib import Path
+
+import pytest
 
 HERE = Path(__file__).parent
 ROOT = HERE.parent
@@ -11,127 +12,149 @@ import testdata
 from src import tclib3
 
 
-class TestLib(unittest.TestCase):
-    def test_adjust_df_frames(self):
-        for _, frame in testdata.RANDOM_DF_TCS_2997:
-            adjusted_frame = tclib3.helpers.adjust_df_frames(frame, 29.97, True)
-            original_frame = tclib3.helpers.adjust_df_frames(adjusted_frame, 29.97, False)
-            self.assertEqual(frame, original_frame)
+def test_adjust_df_frames():
+    for _, frame in testdata.RANDOM_DF_TCS_2997:
+        adjusted_frame = tclib3.helpers.adjust_df_frames(frame, 29.97, True)
+        original_frame = tclib3.helpers.adjust_df_frames(adjusted_frame, 29.97, False)
+        assert frame == original_frame
 
-    def test_frames_to_tc(self):
-        for tc_str, frame in testdata.RANDOM_DF_TCS_2997:
-            converted_frame = tclib3.frames_to_tc(frame, 29.97, True)
-            self.assertEqual(converted_frame, tc_str)
+def test_frames_to_tc():
+    for tc_str, frame in testdata.RANDOM_DF_TCS_2997:
+        converted_frame = tclib3.frames_to_tc(frame, 29.97, True)
+        assert converted_frame == tc_str
 
-        for tc_str, frame in testdata.RANDOM_TCS_2997:
-            converted_frame = tclib3.frames_to_tc(frame, 29.97, False)
-            self.assertEqual(converted_frame, tc_str)
+    for tc_str, frame in testdata.RANDOM_TCS_2997:
+        converted_frame = tclib3.frames_to_tc(frame, 29.97, False)
+        assert converted_frame == tc_str
 
-        for tc_str, frame in testdata.RANDOM_TCS_2398:
-            converted_frame = tclib3.frames_to_tc(frame, 23.976, False)
-            self.assertEqual(converted_frame, tc_str)
+    for tc_str, frame in testdata.RANDOM_TCS_2398:
+        converted_frame = tclib3.frames_to_tc(frame, 23.976, False)
+        assert converted_frame == tc_str
 
-    def test_frames_to_tc_to_frames(self):
-        for _, frame in testdata.RANDOM_DF_TCS_2997:
-            tc = tclib3.frames_to_tc(frame, 29.97, True)
-            converted_frames = tclib3.tc_to_frames(tc, 29.97, force_df=True)
-            self.assertEqual(converted_frames, frame)
+def test_frames_to_tc_to_frames():
+    for _, frame in testdata.RANDOM_DF_TCS_2997:
+        tc = tclib3.frames_to_tc(frame, 29.97, True)
+        converted_frames = tclib3.tc_to_frames(tc, 29.97, force_df=True)
+        assert converted_frames == frame
 
-        for _, frame in testdata.RANDOM_TCS_2997:
-            tc = tclib3.frames_to_tc(frame, 29.97, False)
-            converted_frames = tclib3.tc_to_frames(tc, 29.97)
-            self.assertEqual(converted_frames, frame)
+    for _, frame in testdata.RANDOM_TCS_2997:
+        tc = tclib3.frames_to_tc(frame, 29.97, False)
+        converted_frames = tclib3.tc_to_frames(tc, 29.97)
+        assert converted_frames == frame
 
-        for _, frame in testdata.RANDOM_TCS_2398:
-            tc = tclib3.frames_to_tc(frame, 23.976, False)
-            converted_frames = tclib3.tc_to_frames(tc, 23.976)
-            self.assertEqual(converted_frames, frame)
+    for _, frame in testdata.RANDOM_TCS_2398:
+        tc = tclib3.frames_to_tc(frame, 23.976, False)
+        converted_frames = tclib3.tc_to_frames(tc, 23.976)
+        assert converted_frames == frame
 
-    def test_tc_to_frames(self):
-        for tc_str, frame in testdata.RANDOM_DF_TCS_2997:
-            converted_frame = tclib3.tc_to_frames(tc_str, 29.97, force_df=True)
-            self.assertEqual(converted_frame, frame)
+    for _, frame in testdata.RANDOM_TCS_2398:
+        tc = tclib3.frames_to_tc(frame, 24, False)
+        converted_frames = tclib3.tc_to_frames(tc, 24)
+        assert converted_frames == frame
 
-        for tc_str, frame in testdata.RANDOM_DF_TCS_2997:
-            converted_frame = tclib3.tc_to_frames(tc_str, 29.97)
-            self.assertEqual(converted_frame, frame)
+def test_tc_to_frames():
+    for tc_str, frame in testdata.RANDOM_DF_TCS_2997:
+        converted_frame = tclib3.tc_to_frames(tc_str, 29.97, force_df=True)
+        assert converted_frame == frame
 
-        for tc_str, frame in testdata.RANDOM_TCS_2997:
-            converted_frame = tclib3.tc_to_frames(tc_str, 29.97)
-            self.assertEqual(converted_frame, frame)
+    for tc_str, frame in testdata.RANDOM_DF_TCS_2997:
+        converted_frame = tclib3.tc_to_frames(tc_str, 29.97)
+        assert converted_frame == frame
 
-        for tc_str, frame in testdata.RANDOM_TCS_2398:
-            converted_frame = tclib3.tc_to_frames(tc_str, 23.976)
-            self.assertEqual(converted_frame, frame)
+    for tc_str, frame in testdata.RANDOM_TCS_2997:
+        converted_frame = tclib3.tc_to_frames(tc_str, 29.97)
+        assert converted_frame == frame
 
-    def test_tc_to_frames_to_tc(self):
-        for tc_str, _ in testdata.RANDOM_DF_TCS_2997:
-            frames = tclib3.tc_to_frames(tc_str, 29.97, force_df=True)
-            converted_tc = tclib3.frames_to_tc(frames, 29.97, True)
-            self.assertEqual(converted_tc, tc_str)
+    for tc_str, frame in testdata.RANDOM_TCS_2398:
+        converted_frame = tclib3.tc_to_frames(tc_str, 23.976)
+        assert converted_frame == frame
 
-        for tc_str, _ in testdata.RANDOM_DF_TCS_2997:
-            frames = tclib3.tc_to_frames(tc_str, 29.97)
-            converted_tc = tclib3.frames_to_tc(frames, 29.97, False)
-            self.assertNotEqual(converted_tc, tc_str)
+def test_tc_to_frames_to_tc():
+    for tc_str, _ in testdata.RANDOM_DF_TCS_2997:
+        frames = tclib3.tc_to_frames(tc_str, 29.97, force_df=True)
+        converted_tc = tclib3.frames_to_tc(frames, 29.97, True)
+        assert converted_tc == tc_str
 
-        for tc_str, _ in testdata.RANDOM_TCS_2997:
-            frames = tclib3.tc_to_frames(tc_str, 29.97)
-            converted_tc = tclib3.frames_to_tc(frames, 29.97, False)
-            self.assertEqual(converted_tc, tc_str)
+    for tc_str, _ in testdata.RANDOM_DF_TCS_2997:
+        frames = tclib3.tc_to_frames(tc_str, 29.97)
+        converted_tc = tclib3.frames_to_tc(frames, 29.97, False)
+        assert not (converted_tc == tc_str)
 
-        for tc_str, _ in testdata.RANDOM_TCS_2398:
-            frames = tclib3.tc_to_frames(tc_str, 23.976)
-            converted_tc = tclib3.frames_to_tc(frames, 23.976, False)
-            self.assertEqual(converted_tc, tc_str)
+    for tc_str, _ in testdata.RANDOM_TCS_2997:
+        frames = tclib3.tc_to_frames(tc_str, 29.97)
+        converted_tc = tclib3.frames_to_tc(frames, 29.97, False)
+        assert converted_tc == tc_str
 
-    def test_frames_to_ms(self):
-        for frames, ms in testdata.RANDOM_MS_2997:
-            converted_ms = tclib3.frames_to_ms(frames, 29.97)
-            self.assertEqual(converted_ms, ms)
+    for tc_str, _ in testdata.RANDOM_TCS_2398:
+        frames = tclib3.tc_to_frames(tc_str, 23.976)
+        converted_tc = tclib3.frames_to_tc(frames, 23.976, False)
+        assert converted_tc == tc_str
 
-        for frames, ms in testdata.RANDOM_MS_2398:
-            converted_ms = tclib3.frames_to_ms(frames, 23.976)
-            self.assertEqual(converted_ms, ms)
+def test_frames_to_ms():
+    for frames, ms in testdata.RANDOM_MS_2997:
+        converted_ms = tclib3.frames_to_ms(frames, 29.97)
+        assert converted_ms == ms
 
-    def test_ms_to_frames(self):
-        for frames, ms in testdata.RANDOM_MS_2997:
-            converted_frames = tclib3.ms_to_frames(ms, 29.97)
-            self.assertEqual(converted_frames, frames)
+    for frames, ms in testdata.RANDOM_MS_2398:
+        converted_ms = tclib3.frames_to_ms(frames, 23.976)
+        assert converted_ms == ms
 
-        for frames, ms in testdata.RANDOM_MS_2398:
-            converted_frames = tclib3.ms_to_frames(ms, 23.976)
-            self.assertEqual(converted_frames, frames)
+    for frames, ms in testdata.RANDOM_MS_24:
+        converted_ms = tclib3.frames_to_ms(frames, 24)
+        assert converted_ms == ms
 
-    def test_frames_to_ms_to_frames(self):
-        for frames, _ in testdata.RANDOM_MS_2997:
-            ms = tclib3.frames_to_ms(frames, 29.97)
-            converted_frames = tclib3.ms_to_frames(ms, 29.97)
-            self.assertEqual(converted_frames, frames)
+def test_ms_to_frames():
+    for frames, ms in testdata.RANDOM_MS_2997:
+        converted_frames = tclib3.ms_to_frames(ms, 29.97)
+        assert converted_frames == frames
 
-        for frames, _ in testdata.RANDOM_MS_2398:
-            ms = tclib3.frames_to_ms(frames, 23.976)
-            converted_frames = tclib3.ms_to_frames(ms, 23.976)
-            self.assertEqual(converted_frames, frames)
+    for frames, ms in testdata.RANDOM_MS_2398:
+        converted_frames = tclib3.ms_to_frames(ms, 23.976)
+        assert converted_frames == frames
 
-    def test_ms_to_frames_to_ms(self):
-        for _, ms in testdata.RANDOM_MS_2997:
-            frames = tclib3.ms_to_frames(ms, 29.97)
-            converted_ms = tclib3.frames_to_ms(frames, 29.97)
-            self.assertEqual(converted_ms, ms)
+    for frames, ms in testdata.RANDOM_MS_24:
+        converted_frames = tclib3.ms_to_frames(ms, 24)
+        assert converted_frames == frames
 
-        for _, ms in testdata.RANDOM_MS_2398:
-            frames = tclib3.ms_to_frames(ms, 23.976)
-            converted_ms = tclib3.frames_to_ms(frames, 23.976)
-            self.assertEqual(converted_ms, ms)
+def test_frames_to_ms_to_frames():
+    for frames, _ in testdata.RANDOM_MS_2997:
+        ms = tclib3.frames_to_ms(frames, 29.97)
+        converted_frames = tclib3.ms_to_frames(ms, 29.97)
+        assert converted_frames == frames
 
-    def test_duration(self):
-        for start, end, df_dur, ndf_dur in testdata.RANDOM_DUR_2997:
-            self.assertEqual(tclib3.duration(start, end, 29.97, True), df_dur)
-            self.assertEqual(tclib3.duration(start, end, 29.97, False), ndf_dur)
+    for frames, _ in testdata.RANDOM_MS_2398:
+        ms = tclib3.frames_to_ms(frames, 23.976)
+        converted_frames = tclib3.ms_to_frames(ms, 23.976)
+        assert converted_frames == frames
 
-        for start, end, dur in testdata.RANDOM_DUR_2398:
-            self.assertEqual(tclib3.duration(start, end, 23.976), dur)
+    for frames, _ in testdata.RANDOM_MS_24:
+        ms = tclib3.frames_to_ms(frames, 24)
+        converted_frames = tclib3.ms_to_frames(ms, 24)
+        assert converted_frames == frames
 
-if __name__ == "__main__":
-    unittest.main()
+def test_ms_to_frames_to_ms():
+    for _, ms in testdata.RANDOM_MS_2997:
+        frames = tclib3.ms_to_frames(ms, 29.97)
+        converted_ms = tclib3.frames_to_ms(frames, 29.97)
+        assert converted_ms == ms
+
+    for _, ms in testdata.RANDOM_MS_2398:
+        frames = tclib3.ms_to_frames(ms, 23.976)
+        converted_ms = tclib3.frames_to_ms(frames, 23.976)
+        assert converted_ms == ms
+
+    for _, ms in testdata.RANDOM_MS_24:
+        frames = tclib3.ms_to_frames(ms, 24)
+        converted_ms = tclib3.frames_to_ms(frames, 24)
+        assert converted_ms == ms
+
+def test_duration():
+    for start, end, df_dur, ndf_dur in testdata.RANDOM_DUR_2997:
+        assert tclib3.duration(start, end, 29.97, True) == df_dur
+        assert tclib3.duration(start, end, 29.97, False) == ndf_dur
+
+    for start, end, dur in testdata.RANDOM_DUR_2398:
+        assert tclib3.duration(start, end, 23.976) == dur
+
+    with pytest.raises(ValueError):
+        tclib3.duration("00:00:00:02", "00:00:00:01", 23.976)
